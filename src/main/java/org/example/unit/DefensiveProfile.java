5cfba9b93ced81ec6a0f0bdbaef88932575624ce
markomatovic <ermax5d@gmail.com>
2023-11-26 18:51:08 +0100

    removed old code

--- src/main/java/org/example/unit/DefensiveProfile.java
+++
@@ -1,7 +0,0 @@
-package org.example.unit;
-
-public record DefensiveProfile(int hp,
-                               int defensiveSkill,
-                               int resilience,
-                               int armour) {
-}
--- src/main/java/org/example/unit/OffensiveProfile.java
+++
@@ -1,8 +0,0 @@
-package org.example.unit;
-
-public record OffensiveProfile(int attacks,
-                               int offensiveSkill,
-                               int strength,
-                               int armour_penetration,
-                               int agility) {
-}
